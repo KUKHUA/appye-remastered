91c8439c010f4151d2c3e3d67177119ceb1c53d9
Your Name <you@example.com>
2025-01-23 18:47:19 -0500

    testing installer

--- installerScripts/installer.js
+++ installerScripts/installer.js
@@ -1,1 +1,25 @@
-let mirror = new URL("")
+class installer{
+    constructor(){
+        this.mirror = new URL("https://raw.githack.com/KUKHUA/appye-remastered/tree/main");
+        this.fileSystem = new OPFSFileSystem("");
+        this.fileList = new URL("files.json", this.mirror);
+    }
+
+    async install(){
+        await this.fileSystem.init();
+        this.fileList = await fetch(this.fileList);
+        this.fileList = await this.fileList.json();
+        this.fileList.files.forEach(async element => {
+            let fileURL = new URL(element, this.mirror);
+            let fileBlob = fetch(fileURL).then(response => response.blob());
+            await this.fileSystem.createFile(element, fileBlob);
+        });
+        await this.addServiceWorker();
+    }
+
+    async addServiceWorker(){
+        if('serviceWorker' in navigator){
+            navigator.serviceWorker.register('fileSystem/system/script/serviceWorker.js');
+        }
+    }
+}
