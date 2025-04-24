5d2ec53757ca2bc2a73e2e1a6cc8f4e1d63a14e9
Your Name <you@example.com>
2025-04-23 20:07:40 -0400

    style: format code in installer.js and index.html for improved readability

--- fileSystem/script/installer.js
+++ fileSystem/script/installer.js
@@ -1,32 +1,34 @@
-class installer{
-    constructor(){
-        this.mirror = new URL("https://raw.githack.com/KUKHUA/appye-remastered/main/");
-        this.fileSystem = new OPFSFileSystem("root");
-        this.fileList = new URL("files.json", this.mirror);
-    }
+class installer {
+  constructor() {
+    this.mirror = new URL(
+      "https://raw.githack.com/KUKHUA/appye-remastered/main/"
+    );
+    this.fileSystem = new OPFSFileSystem("root");
+    this.fileList = new URL("files.json", this.mirror);
+  }
 
-    async install(){
-        await this.fileSystem.init();
-        this.fileList = await fetch(this.fileList);
-        this.fileList = await this.fileList.json();
-        this.fileList.files.forEach(async element => {
-            let fileURL = this.mirror.href + element;
-            console.log(fileURL);
-            let fileBlob = fetch(fileURL);
-            fileBlob = await fileBlob;
-            fileBlob = await fileBlob.blob();
-            await this.fileSystem.createFile(element, fileBlob);
-        });
-        await this.addServiceWorker();
-    }
+  async install() {
+    await this.fileSystem.init();
+    this.fileList = await fetch(this.fileList);
+    this.fileList = await this.fileList.json();
+    this.fileList.files.forEach(async (element) => {
+      let fileURL = this.mirror.href + element;
+      console.log(fileURL);
+      let fileBlob = fetch(fileURL);
+      fileBlob = await fileBlob;
+      fileBlob = await fileBlob.blob();
+      await this.fileSystem.createFile(element, fileBlob);
+    });
+    await this.addServiceWorker();
+  }
 
-    isInstalled(){
-        return this.fileSystem.hasFile("fileSystem/system.json");
-    }
+  isInstalled() {
+    return this.fileSystem.hasFile("fileSystem/system.json");
+  }
 
-    async addServiceWorker(){
-        if('serviceWorker' in navigator){
-            navigator.serviceWorker.register('fileSystem/script/serviceWorker.js');
-        }
+  async addServiceWorker() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("serviceWorker.js");
     }
+  }
 }
